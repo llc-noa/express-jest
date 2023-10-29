58631efc7f4519f9a1a7d06a8387a9df5883b059
rsugaya <rsugaya@llc-noa.jp>
2023-10-29 15:02:08 +0900

    追加

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const { Get } = require('./root');
+
+router.get('/', Get);
+
+app.use('/', router);
+
+app.listen(3000);
---
+++ tests/root.test.js
@@ -0,0 +1,18 @@
+const { Get } = require('../src/root');
+
+test('status 200 with id', () => {
+    const req = {
+        query: {
+            id: 'ezaki',
+        },
+    };
+
+    const res = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+    };
+
+    Get(req, res);
+
+    expect(res.status.mock.calls[0][0]).toBe(200);
+});
