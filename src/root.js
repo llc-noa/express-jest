84e64e0e95795cdc65a1a73c575cf560b15d3596
rsugaya <rsugaya@llc-noa.jp>
2023-11-18 14:52:00 +0900

    jest実装

--- src/root.js
+++ src/root.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     if (req.query.id) {
-        return res.status(200).send(`Hi! ${req.query.id}`);
+        res.status(200).send({
+            id: req.query.id,
+        });
     }
 
     res.status(400).send('Who are you?');
--- tests/root.test.js
+++ tests/root.test.js
@@ -4,9 +4,14 @@ const app = require('../app.js');
 const path = '/api/root';
 
 jest.setTimeout(10000);
+describe('正常', () => {
+    test('レスポンス確認', async () => {
+        const result = await request(app).get(path).query({ id: 'ezaki' });
 
-test('status 200 with id', async () => {
-    const result = await request(app).get(path).query({ id: 'ezaki' });
-
-    expect(result.statusCode).toBe(200);
+        expect(result.statusCode).toBe(200);
+        expect(result.statusCode).toBe(200);
+        expect(result.body).toStrictEqual({
+            id: 'ezaki',
+        });
+    });
 });
